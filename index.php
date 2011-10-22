0c8bbeafd4cb0a85fbac2460bb38dede5e61b5cd
Sameen Jalal <sameenjalal@gmail.com>
2011-10-22 14:15:40 -0700

    f

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>sameenjalal.com</title>
+		<meta http-equiv="Content-Language" content="English" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+	</head>
+
+	<body>
+		<div id="wrap">
+			<div id="header">
+				<h1><a href="#">sameenjalal.com</a></h1>
+				<h2>Sameen Jalal's Personal Website</h2>
+			</div>
+
+			<div id="menu">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Photos</a></li>
+					<li><a href="#">Links</a></li>
+					<li><a href="#">Forum</a></li>
+					<li><a href="#">Free Stuff</a></li>
+					<li><a href="#">Downloads</a></li>
+					<li><a href="#">Products</a></li>
+					<li><a href="#">SiteMap</a></li>
+				</ul>
+			</div>
+
+			<div id="content">
+				<div class="right"> 
+
+					<h2><a href="#">Present</a></h2>
+					<div class="articles">
+						<p>Sameen is currently being awesome at Rutgers University
+						<br /><br />
+						<img src="images/pic.jpg" alt="Example pic" style="border: 3px solid #ccc;" />
+						<br /><br />
+						Donec nulla. Aenean eu augue ac nisl tincidunt rutrum. Proin erat justo, pharetra eget, posuere at, malesuada 
+						et, nulla. Donec pretium nibh sed est faucibus suscipit. Nunc nisi. Nullam vehicula. In ipsum lorem, bibendum sed, 
+						consectetuer et, gravida id, erat. Ut imperdiet, leo vel condimentum faucibus, risus justo feugiat purus, vitae 
+						congue nulla diam non urna.
+					</div>
+
+					<h2><a href="#">Recent Activity</a></h2>
+
+					<div class="articles">
+						Donec nulla. Aenean eu augue ac nisl tincidunt rutrum. Proin erat justo, pharetra eget, posuere at, malesuada 
+						et, nulla. Donec pretium nibh sed est faucibus suscipit. Nunc nisi. Nullam vehicula. In ipsum lorem, bibendum sed, 
+						consectetuer et, gravida id, erat. Ut imperdiet, leo vel condimentum faucibus, risus justo feugiat purus, vitae 
+						congue nulla diam non urna.
+					</div>
+				</div>
+
+					<div class="left"> 
+
+						<h2>Academics:</h2>
+						<ul>
+							<li><a href="#">Rutgers</a></li> 
+							<li><a href="#">Research</a></li> 
+							<li><a href="#">Side Projects</a></li> 
+							<li><a href="#">Competitions</a></li>
+							<li><a href="#">SuperCool Creations</a></li> 
+							<li><a href="#">Resume</a></li> 
+						</ul>
+
+						<h2>Extracurriculars:</h2>
+						<ul>
+							<li><a href="#">Undergraduate Computer Science</a></li> 
+							<li><a href="#">Linux User Group</a></li> 
+							<li><a href="#">Women in CS</a></li> 
+							<li><a href="#">Table Tennis</a></li>
+							<li><a href="#">Salsa</a></li> 
+							<li><a href="#">Bengali Students Association</a></li> 
+							<li><a href="#">Chi Psi Fraternity</a></li> 
+						</ul>
+
+						<h2>Contact:</h2>
+						<ul>
+							<li><a href="#">Facebook</a></li> 
+							<li><a href="#">LinkedIn</a></li> 
+							<li><a href="#">Twitter</a></li> 
+							<li><a href="#">Email Me</a></li>
+						</ul>
+
+					</div>
+					<div style="clear: both;"> </div>
+				</div>
+
+			<div id="footer">
+				<a href="http://sameenjalal.com/" target="_blank">Last updated November 28th 2010</a>
+			</div>
+
+			<div id="musthave">
+				<a href="http://www.templatesold.com/" target="_blank">.</a><a href="http://www.free-css-templates.com/" target="_blank">.</a>
+			</div>
+		</div>
+
+	</body>
+</html>
